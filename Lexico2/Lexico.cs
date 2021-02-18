f48d81a996219b2f837d9b454649ff6312ef6928
Luis Talavera <l19141205@queretaro.tecnm.mx>
2021-02-18 12:44:53 -0600

    Agregar archivos de proyecto.

---
+++ Lexico2/Lexico.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Lexico2
+{
+    class Lexico : Token, IDisposable
+    {
+        private StreamReader archivo;
+        private StreamWriter bitacora;
+
+        public Lexico()
+        {
+            Console.WriteLine("Compilando prueba.txt");
+
+            if (File.Exists("C:\\Archivos\\prueba.txt"))
+            {
+                archivo = new StreamReader("C:\\Archivos\\prueba.txt");
+                bitacora = new StreamWriter("C:\\Archivos\\prueba.log");
+                bitacora.AutoFlush = true;
+
+                bitacora.WriteLine("Archivo: prueba.txt");
+                bitacora.WriteLine("Directorio: C:\\Archivos");
+            }
+            else
+            {
+                throw new Exception("El archivo prueba.txt no existe.");
+            }
+
+        }
+        //~Lexico()
+        public void Dispose()
+        {
+            Console.WriteLine("Finaliza compilacion de prueba.txt");
+            CerrarArchivos();
+        }
+
+        private void CerrarArchivos()
+        {
+            archivo.Close();
+            bitacora.Close();
+        }
+
+        public void NextToken()
+        {
+            char c;
+            string palabra = "";
+
+            setContenido(palabra);
+            bitacora.WriteLine("Token = " + getContenido());
+            bitacora.WriteLine("Clasificacion = " + getClasificacion());
+        }
+
+        public bool FinDeArchivo()
+        {
+            return archivo.EndOfStream;
+        }
+    }
+}
